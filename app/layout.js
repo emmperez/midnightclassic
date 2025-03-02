2cc969540ccc2bfbc4ccd6e7662b876311319f6d
emmanuel <emmanueleperez@icloud.com>
2025-03-01 23:46:26 -0500

    enabled analytics

--- app/layout.js
+++ app/layout.js
@@ -1,4 +1,5 @@
 import "./globals.css";
+import { Analytics } from "@vercel/analytics/react"
 
 export const metadata = {
   title: "porfoliooohhhhhh",
@@ -10,6 +11,7 @@ export default function RootLayout({ children }) {
     <html lang="en">
       <body className="custom-font no-scrollbar overflow-hidden">
         {children}
+        <Analytics />
       </body>
     </html>
   );
