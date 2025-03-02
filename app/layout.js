aab15a8d27a212f50ceadbe135ef422cfa4c0dfa
emmanuel <emmanueleperez@icloud.com>
2025-03-01 23:39:49 -0500

    overflow fix, nav height update

--- app/layout.js
+++ app/layout.js
@@ -8,7 +8,7 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className="custom-font no-scrollbar overflow-y-hidden">
+      <body className="custom-font no-scrollbar overflow-hidden">
         {children}
       </body>
     </html>
