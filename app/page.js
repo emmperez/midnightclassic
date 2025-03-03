1cf88beecfa881295a7bf2f2d3837bd262ffdb44
emmanuel <emmanueleperez@icloud.com>
2025-03-03 07:41:24 -0500

    sidebar update with tablet fix

--- app/page.js
+++ app/page.js
@@ -2,15 +2,14 @@
 import Header from "./components/Header/Header";
 import Hero from "./components/Hero/Hero";
 import Loader from "./components/Loader/Loader";
-import Aside from "./components/Hero/Aside";
+
 export default function Home() {
 
   return (
     <>
       <Loader />
       <Header />
-      {/* <Hero /> */}
-      <Aside />
+      <Hero />
     </>
   );
 }
